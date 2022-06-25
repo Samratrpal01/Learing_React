86e1b435c02f5d1dcbe6d79cbcfeda4b9efd6f26
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-06-25 18:59:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import "./styles.css";
 import React, { useState } from "react";
 
-var headingtext = "inside out";
-var color = "red";
-var name = "Samrat Rahul Pal";
+const emojiDictionary = {
+  "😂": "Face with tears of joy",
+  "💕": "Friend love",
+  "❤️": "Real Heart",
+  "👌": "Nice",
+  "☺️": "Cute Smile",
+  "😉": "winkling face",
+  "😊": "Normal Smile",
+  "😊": "Sad",
+  "😒": "Unamused Face"
+};
 //name=prompt("Enter Your name");
 export default function App() {
-  const [likeCounter, setLikeCounter] = useState(0);
-  var count = 0;
-  function likeClickHandler() {
-    var newlikeCounter = likeCounter + 1;
-    setLikeCounter(newlikeCounter);
+  const [meaning, setMeaning] = useState("");
+  function emojiHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "we don't have this in our database";
+    }
+    setMeaning(meaning);
   }
   return (
     <div className="App">
-      <h1>TonyStark</h1>
-      <h1 style={{ backgroundColor: "blue" }}>
-        Welcome<span style={{ backgroundColor: "red" }}> {name}</span>
-      </h1>
-      <h2>Hii How are you</h2>
-      <h3 style={{ backgroundColor: color }}>{headingtext}</h3>
-      <button onClick={likeClickHandler}>Like Me!</button>
-      <h1>you click it </h1>
-      {likeCounter}
+      <h1>Emoji Interpreter</h1>
+      <input onChange={emojiHandler} />
+      <h2>"😂 ❤️ 👌 💕 😊 😉 ☺️ 😒 "</h2>
+      <h2>{meaning}</h2>
     </div>
   );
 }
